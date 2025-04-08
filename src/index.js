9c2c11f049d5f905a1eafe81240196aaf14ae247
Ashita Gupta <ashita.600195@gmail.com>
2025-04-08 13:12:21 +0530

    Switched to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom'; // ✅ Import HashRouter
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,13 +8,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <Router> {/* ✅ Replace BrowserRouter with HashRouter */}
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Optional: log web vitals
 reportWebVitals();
